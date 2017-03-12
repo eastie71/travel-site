ed568da05608fc21ff40e66253318c7f26b1bc3b
Craig Eastwood <craigdavideastwood@gmail.com>
2017-03-12 15:35:42 +1100

    Added various comments through code and tasks - up to about middle of web page content

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,17 +1,31 @@
 var gulp = require('gulp');
+// The postcss plugin does not do much out of the boc - you still need other plugins added to it as well
 var postcss = require('gulp-postcss');
+
+// autoprofixer used to handle vendor prefixes for browsers - eg. webkit-.... 
 var autoprefixer = require('autoprefixer');
+
+// simple-vars allows you to use variables in CSS eg. $mainBlueColor: #232355;
 var cssvars = require('postcss-simple-vars');
+
+// nested allows you to write nested CSS 
 var nestedcss = require('postcss-nested');
+
+// css-import allows you to make use of import statements to include multiple css files to build
+// one large file for the final web site
 var cssimport = require('postcss-import');
+
+// mixins package allows you create reusable bits of code (CSS)
+// See "_mixins.css" file
 var mixins = require('postcss-mixins');
 var hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
+	// postcss expects an array of plugins
 	return gulp.src('./app/assets/styles/styles.css')
 			.pipe(postcss([cssimport, mixins, cssvars, nestedcss, hexrgba, autoprefixer]))
 			.on('error', function (errorInfo) {
-				// Log some info on the error that occurred.
+				// Log some info on the error that occurred. (so you can debug problems in your css)
 				console.log(errorInfo.toString());
 				// this is if the postcss task receives an error, then this allows the gulp task to continue (without bombing out)
 				this.emit('end');
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
+// Gulp watch plugin is used to "watch" files for changes, hence can be used to trigger events.
 var watch = require('gulp-watch');
+// not requiring in the whole package - just the create method
 var browsersync = require('browser-sync').create();
 
 gulp.task('watch', function() {
+	// browsersync creates a webserver on your computer - need to point it the "app" folder as
+	// this is where the "index.html" file lives. (ie baseDir = 'app')
+	// By default browser shows notification messages whenever CSS is injected, the "notify: false"
+	// config removes this from happening.
 	browsersync.init({
 		notify: false,
 		server : {
@@ -10,11 +16,13 @@ gulp.task('watch', function() {
 		}
 	});
 
+	// First argument to watch is the filename to watch for changes
+	// Second argument is what to do when the file changes. (call an anonymous function here)
 	watch('./app/index.html', function() {
-		//gulp.start('html');
 		browsersync.reload();
 	});
 
+	// Watch for changes to any css files under styles folder (including ANY sub-folders)
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('cssInject');
 	});
@@ -24,6 +32,9 @@ gulp.task('watch', function() {
 	});
 });
 
+// this injects the CSS without even performing a refresh of the page! 
+// need to use "return" here as it is "gulp.src()" is an async function
+// Before running the inject - it must perform any POSTCSS required (in "styles" task)
 gulp.task('cssInject', ['styles'], function () {
 	return gulp.src('./app/temp/styles/styles.css')
 			.pipe(browsersync.stream());
