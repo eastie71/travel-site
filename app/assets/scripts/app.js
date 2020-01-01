58ef853596a15ca5b51a995cbf9a39cab5100305
Craig Eastwood <craigdavideastwood@gmail.com>
2020-01-02 10:34:02 +1100

    Added comments in describing the Modal JS loading code

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -10,15 +10,22 @@ new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new StickyHeader()
 
 // Setup the "Get in Touch" Modal
+// This is done this way as a performance improvement. Rather than have the JS for the modal download
+// to the users browser everytime the user opens the page - this code only downloads if the user 
+// clicks on the "Get in Touch" (modal) button.
 var modal
 document.querySelectorAll(".open-modal").forEach(el => {
   el.addEventListener("click", e => {
     e.preventDefault()
     if (typeof modal == "undefined") {
+      // This is a "promise" call ie. call "import", once completed call "then", else if fails call "catch"
+      // The comment code here that contains "webpackChunkName" actually sets the name of the JS file that is downloaded!!
+      // The JS file name will be "modal.bundled.js" 
       import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
         modal = new x.default()
+        // Need to set a timeout to allow browser to setup the modal object
         setTimeout(() => modal.openModal(), 20)
-      }).catch(() => console.log("There was a problem."))
+      }).catch(() => console.log("There was a problem loading the modal for 'Get in Touch'"))
     } else {
       modal.openModal()
     }
