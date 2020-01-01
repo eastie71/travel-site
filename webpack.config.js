f9f781c2e81fe8ee8738d75e0abcb2164e35b27f
Craig Eastwood <craigdavideastwood@gmail.com>
2020-01-01 14:51:51 +1100

    Converted to webpack for dev building JS and CSS - removed sprite CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
-// babel allows you to make use of modern ES6 js such as classes, etc,
-// while supporting older browsers
+const path = require('path')
+
+const postCSSPlugins = [
+	require('postcss-import'),
+	require('postcss-mixins'),
+	require('postcss-simple-vars'),
+	require('postcss-nested'),
+	require('postcss-hexrgba'),
+	require('autoprefixer')
+]
+
 module.exports = {
-	entry: {
-		app: "./app/assets/scripts/app.js",
-		Vendor: "./app/assets/scripts/Vendor.js"
-	},
+	entry: "./app/assets/scripts/App.js",
 	output: {
-		path: "./app/temp/scripts",
-		filename: "[name].js"
+		filename: 'bundled.js',
+		path: path.resolve(__dirname, 'app')
 	},
+	mode: 'development',
+	watch: true,
 	module: {
-		loaders: [
+		rules: [
 			{
-				loader: 'babel-loader',
-				query: {
-					presets: ['es2015']
-				},
-				test: /\.js$/,
-				exclude: /node_modules/
+				test: /\.css$/i,
+				/* css-loader has added option '?url=false' so that it disables handling of images in CSS (eg. background images) */
+				use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins} }]
 			}
 		]
 	}
