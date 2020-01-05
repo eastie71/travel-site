7bf25839ecfe1ae0eaa875876a3c6968fa54c855
Craig Eastwood <craigdavideastwood@gmail.com>
2020-01-05 23:34:21 +1100

    Updated cloud function for password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the Super Secret Area!"
-    })
+    const secretContent = `
+        <h3>Congratulations! You have accessed the Secret Area!</h3>
+        <p>Did you know that the Richmond Tigers won the AFL premiership in 2017 AND 2019! <strong>WOO HOO!</strong></p>
+    `
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "secretpassword") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+    
 }
