36bcff8ff90e1786070c25645f8d3f6f01adc376
Craig Eastwood <craigdavideastwood@gmail.com>
2020-01-23 18:26:08 +1100

    Moved secret area file for app

--- app/cloud-functions/secret-area.js
+++ app/cloud-functions/secret-area.js
@@ -1,4 +1,17 @@
 exports.handler = function(event, context, callback) {
+    const headers = {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Headers" : "Content-Type"
+    }
+
+    if (event.httpMethod !== "POST") {
+        return callback(null, {
+            statusCode: 200,
+            headers,
+            body: "This was NOT a POST request"
+        })
+    }
+
     const secretContent = `
         <h3>Congratulations! You have accessed the Secret Area!</h3>
         <p>Did you know that the Richmond Tigers won the AFL premiership in 2017 AND 2019! <strong>WOO HOO!</strong></p>
@@ -14,12 +27,13 @@ exports.handler = function(event, context, callback) {
     if (body.password == "secretpassword") {
         callback(null, {
             statusCode: 200,
+            headers,
             body: secretContent
         })
     } else {
         callback(null, {
-            statusCode: 401
+            statusCode: 401,
+            headers
         })
     }
-    
 }
